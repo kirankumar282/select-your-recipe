4084292a9fc12dc5c29f83f547be7abbf7c0722b
Kiran Kumar Bolemari <49384207+kirankumar282@users.noreply.github.com>
2022-07-10 10:56:23 +0000

    added login,signup and about home

--- app.py
+++ app.py
@@ -2,50 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-RECIPE=[
+About=[
   {
     'item':1,
     'dish':'mix-veg curry',
     'spicy-level':'Normal',
     'members': 5
   },
-  {
-    'item':2,
-    'dish':'chicken pulusu',
-    'spicy-level':'medium',
-    'members': 8
-  },
-  {
-    'item':3,
-    'dish':'Mutton pulusu',
-    'spicy-level':'medium',
-    'members': 10
-  },
-  {
-    'item':4,
-    'dish':'Mutton-Kheema-balls',
-    'spicy-level':'Spicy',
-    'members': 12
-  }
-]
-
-ORISSA=[
-{
-    'item':1,
-    'dish':'Dhalma',
-    'spicy-level':'Normal',
-    'members': 5
-  },
-{
-    'item':2,
-    'dish':'Chenna-poda',
-    'members': 8
-  }
 ]
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html',recipes=RECIPE,states=ORISSA)
+  return render_template('home.html',about=About)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
